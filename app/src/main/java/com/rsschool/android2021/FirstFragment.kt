3936d66d1fa725d30f0b1be18283d581aeacd79f
Anton <neutral@bk.ru>
2021-05-30 17:59:23 +0300

    add: solution

--- app/src/main/java/com/rsschool/android2021/FirstFragment.kt
+++ app/src/main/java/com/rsschool/android2021/FirstFragment.kt
@@ -1,17 +1,27 @@
 package com.rsschool.android2021
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class FirstFragment : Fragment() {
 
     private var generateButton: Button? = null
     private var previousResult: TextView? = null
+    private var maxValue: TextView? = null
+    private var minValue: TextView? = null
+    private var listener: OnFirstFragmentListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = context as OnFirstFragmentListener
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +35,8 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         previousResult = view.findViewById(R.id.previous_result)
         generateButton = view.findViewById(R.id.generate)
+        maxValue = view.findViewById(R.id.max_value)
+        minValue = view.findViewById(R.id.min_value)
 
         val result = arguments?.getInt(PREVIOUS_RESULT_KEY)
         previousResult?.text = "Previous result: ${result.toString()}"
@@ -34,11 +46,21 @@ class FirstFragment : Fragment() {
 
         generateButton?.setOnClickListener {
             // TODO: send min and max to the SecondFragment
+            try {
+                val min = minValue?.text.toString().toInt()
+                val max = maxValue?.text.toString().toInt()
+                if (min >= max || max == 0) {
+                    Toast.makeText(context, "oops, invalid data", Toast.LENGTH_LONG).show()
+                } else {
+                    listener?.onFirstFragmentListener(min, max)
+                }
+            } catch (e: NumberFormatException) {
+                Toast.makeText(context, "oops, invalid data", Toast.LENGTH_LONG).show()
+            }
         }
     }
 
     companion object {
-
         @JvmStatic
         fun newInstance(previousResult: Int): FirstFragment {
             val fragment = FirstFragment()
@@ -46,6 +68,10 @@ class FirstFragment : Fragment() {
             args.putInt(PREVIOUS_RESULT_KEY, previousResult)
             fragment.arguments = args
             return fragment
+        }
+
+        interface OnFirstFragmentListener {
+            fun onFirstFragmentListener(min: Int, max: Int)
         }
 
         private const val PREVIOUS_RESULT_KEY = "PREVIOUS_RESULT"
--- app/src/main/java/com/rsschool/android2021/SecondFragment.kt
+++ app/src/main/java/com/rsschool/android2021/SecondFragment.kt
@@ -1,17 +1,21 @@
 package com.rsschool.android2021
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
+import kotlin.random.Random
 
 class SecondFragment : Fragment() {
 
     private var backButton: Button? = null
     private var result: TextView? = null
+    private var listener: OnSecondFragmentListener? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +23,11 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = context as OnSecondFragmentListener
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -33,24 +42,38 @@ class SecondFragment : Fragment() {
 
         backButton?.setOnClickListener {
             // TODO: implement back
+            goToFirstFragment()
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            goToFirstFragment()
         }
     }
 
     private fun generate(min: Int, max: Int): Int {
         // TODO: generate random number
-        return 0
+        return Random.nextInt(min, max)
+    }
+
+    private fun goToFirstFragment() {
+        val previousNumber = result?.text.toString().toInt()
+        listener?.onSecondFragmentListener(previousNumber)
     }
 
     companion object {
-
         @JvmStatic
         fun newInstance(min: Int, max: Int): SecondFragment {
             val fragment = SecondFragment()
             val args = Bundle()
-
             // TODO: implement adding arguments
-
+            args.putInt(MIN_VALUE_KEY, min)
+            args.putInt(MAX_VALUE_KEY, max)
+            fragment.arguments = args
             return fragment
+        }
+
+        interface OnSecondFragmentListener {
+            fun onSecondFragmentListener(result: Int)
         }
 
         private const val MIN_VALUE_KEY = "MIN_VALUE"
